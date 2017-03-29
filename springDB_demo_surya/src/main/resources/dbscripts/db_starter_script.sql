dbebdd4dd153dce01bab5ccd88430c03b9e59b11
suryaprakash.raj <suryaprakash.raj@DS-448A5BC53CEA.HCLT.CORP.HCL.IN>
2017-03-29 15:39:54 +0530

    Initial commit

---
+++ springDB_demo_surya/src/main/resources/dbscripts/db_starter_script.sql
@@ -0,0 +1,9 @@
+create table user_data(
+  user_id  int(3) primary key,
+  user_name varchar(50),
+  user_email varchar(100)
+  );
+  
+insert into user_data values (1, 'surya1','surya1@webb.com');
+
+select * from user_data;
